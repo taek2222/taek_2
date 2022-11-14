36912c6f26a415809e527d6f8e45c4c1065cd92e
taek2222 <118153233+taek2222@users.noreply.github.com>
2022-11-14 14:20:02 +0900

    Rename EXERCISE 1-7.cpp to CHAPTER01/EXERCISE 1-7.cpp

---
+++ CHAPTER01/EXERCISE 1-7.cpp
@@ -0,0 +1,16 @@
+// EXERCISE 1-7
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int speed = 60;
+	double time = 2.5;
+	double distance;
+
+	distance = speed * time;
+
+	cout << "¼Ó·ÂÀº : " << speed  << "(km/h)" << endl;
+	cout << "½Ã°£Àº : " << time << "(h)" << endl;
+	cout << "°Å¸®´Â : " << distance << "(km)" << endl;
+}
